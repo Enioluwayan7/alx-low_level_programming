7159db34efd6ac29e8710b9cae2c00c2a9f64b2a
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-02-17 15:51:44 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,17 +4,27 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int n;
 
-	for (n = 48; n < 58; n++)
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= 9; tens++)/* prints tens digit*/
 	{
-		putchar(n);
-		if (n != 57)
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
-			putchar(',');
-			putchar(' ');
+			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			{
+				purchar(tens);
+				purchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
