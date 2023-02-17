049540108faf698d875ba79e068cecde57e934f5
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-02-17 15:57:58 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				if (!(ones == '9' && tens == '9'))/*adds comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
