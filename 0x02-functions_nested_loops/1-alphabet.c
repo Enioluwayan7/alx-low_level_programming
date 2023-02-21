83968c9ecc45c9d1d9601a0c2b5c2a5fc642b04f
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-02-21 13:26:42 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make Alphabet
+ * Return: void
+ */
+
+void prin_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
