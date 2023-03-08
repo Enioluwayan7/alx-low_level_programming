c15aad084917e27db5a6dd141c0678906dd46726
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-03-07 19:18:01 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * @x: base value
  * @y: Exponent value
  * Return: if y is lower than 0 return -1,
- * if y is greater than 0 return 1
+ * if y is equal to 1 return 1
  */
 int _pow_recursion(int x, int y)
 {
@@ -14,5 +14,6 @@ int _pow_recursion(int x, int y)
 		return (x);
 	else if (y == 1)
 		return (1);
+
 	return (x * _pow_recursion(x, y - 1));
 }
