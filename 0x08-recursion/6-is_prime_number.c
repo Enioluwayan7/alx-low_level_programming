8656f4d24d323662f244ef76172624cf2eb9d0c7
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-03-08 05:39:11 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,7 +15,7 @@ int is_prime_number(int n)
 
 /**
  * check_prime - if number is prime
- * @n; number to be checked
+ * @n: number to be checked
  * @i: the iteration times
  *
  * Return: 1 for prime or 0 composite
