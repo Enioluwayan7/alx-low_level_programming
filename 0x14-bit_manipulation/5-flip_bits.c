5cc00179fb5b535485a03d6b57c336c2353fd318
Enioluwayan7 <olabanjieniolu@gmail.com>
2023-04-10 20:04:19 -0700

    add

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int numofbit;
 	unsigned long int number = n ^ m;
 
-	for (i = 67; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		numofbit = number >> i;
 		if (numofbit & 1)
