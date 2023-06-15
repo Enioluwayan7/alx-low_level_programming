163c4d0092b419c78c2dc5e5eb2841035121eba7
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-06-15 16:23:58 -0700

    add

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - function that returns the number of
+ * elements in a linked dlistint_t list.
  * @h: pointer to the head of the list
  * Return: number of elements in the list
  */
